df79a71d90be3f8c81b228dac03e3ef43151dbb9
Gueren Drive <gdrive@ga-c1.msk.21-school.ru>
2020-12-24 23:04:43 +0300

    Nginx config started

---
+++ docker/nginx/deleteImage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker rmi main-nginx:v1
---
+++ docker/nginx/startNginxAlpine.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#adduser -g 'Nginx www user' -h /home/www/ wwwgdrive
+
+#[-g]: set general information about the account wwwcbz.
+#[-h]: account home dir.
+#[wwwgdrive]: account name.
+
+nginx -g 'daemon off;'
+
