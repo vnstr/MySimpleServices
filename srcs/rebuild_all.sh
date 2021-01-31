f219df7f66150f2c3313a9fe9b35f75d54fd6c96
vnstr <vnstr.main@gmail.com>
2021-01-31 15:25:03 +0300

    Grafana is ready

--- srcs/rebuild_all.sh
+++ srcs/rebuild_all.sh
@@ -8,7 +8,9 @@
 
 #~/MySimpleServices/srcs/phpmyadmin/deleteImage.sh
 
-~/MySimpleServices/srcs/influxdb/deleteImage.sh
+#~/MySimpleServices/srcs/influxdb/deleteImage.sh
+
+~/MySimpleServices/srcs/grafana/deleteImage.sh
 
 #~/MySimpleServices/srcs/nginx/buildMainNginx.sh &&\
 #~/MySimpleServices/srcs/wordpress/buildWordpressNginx.sh &&\
@@ -22,4 +24,6 @@
 
 #~/MySimpleServices/srcs/phpmyadmin/buildPhpMyAdmin.sh
 
-~/MySimpleServices/srcs/influxdb/buildInfluxdb.sh
+#~/MySimpleServices/srcs/influxdb/buildInfluxdb.sh
+
+~/MySimpleServices/srcs/grafana/buildGrafana.sh
--- srcs/stopServices.sh
+++ srcs/stopServices.sh
@@ -5,5 +5,6 @@ kubectl delete -f ~/MySimpleServices/srcs/wordpress/wordpress.yaml
 kubectl delete -f ~/MySimpleServices/srcs/mysql/mysql.yaml
 kubectl delete -f ~/MySimpleServices/srcs/phpmyadmin/phpmyadmin.yaml
 kubectl delete -f ~/MySimpleServices/srcs/influxdb/influxdb.yaml
+kubectl delete -f ~/MySimpleServices/srcs/grafana/grafana.yaml
 
 sleep 5
